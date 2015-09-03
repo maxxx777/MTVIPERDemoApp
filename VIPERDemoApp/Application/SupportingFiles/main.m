20b275b15aff5d812e1a3bf7265f6085d339ed00
Maxim Tsvetkov <777.maxxx@gmail.com>
2015-09-03 21:14:22 +0600

    File Structure created, PCH File created

---
+++ VIPERDemoApp/Application/SupportingFiles/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VIPERDemoApp
+//
+//  Created by MAXIM TSVETKOV on 03.09.15.
+//  Copyright (c) 2015 MAXIM TSVETKOV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MTAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MTAppDelegate class]));
+    }
+}
