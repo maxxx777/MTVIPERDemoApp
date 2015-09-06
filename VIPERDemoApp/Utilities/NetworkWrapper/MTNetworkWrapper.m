ef2c0f2b14228e078bc248f99996e3b5c711ced4
Maxim Tsvetkov <777.maxxx@gmail.com>
2015-09-06 15:32:03 +0600

    NSURLSession instead of NSURLConnection

--- VIPERDemoApp/Utilities/NetworkWrapper/MTNetworkWrapper.m
+++ VIPERDemoApp/Utilities/NetworkWrapper/MTNetworkWrapper.m
@@ -11,14 +11,6 @@
 #import "JSONKit.h"
 
 @interface MTNetworkWrapper ()
-{
-    NSMutableData *response_;
-    NSInteger statusCode_;
-    NSURLRequest *request_;
-    NSURLConnection *connection_;
-    NSTimer *timer_;
-    BOOL error_;
-}
 
 @property (nonatomic, copy) MTNetworkWrapperRequestCompletionBlock requestCompletionBlock;
 
@@ -31,16 +23,35 @@ enum { kTimeout = 60 };
 - (void)sendHttpRequest:(NSURLRequest *)request
              completion:(MTNetworkWrapperRequestCompletionBlock)completion
 {
-    request_ = request;
     [self setRequestCompletionBlock:completion];
+    __weak MTNetworkWrapperRequestCompletionBlock weakCompletionHandler = self.requestCompletionBlock;
     
-    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
-    
-    connection_ = [[NSURLConnection alloc] initWithRequest:request_ delegate:self];
-    response_   = [[NSMutableData alloc] init];
-    error_      = NO;
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:
+                                  ^(NSData *data, NSURLResponse *response, NSError *error) {
+                                      NSInteger statusCode = [((NSHTTPURLResponse*)response) statusCode];
+                                      NSDictionary *result;
+                                      
+                                      if (statusCode == 200) {
+                                          result = [self convertDataToJSON:data
+                                                                 withError:&error];
+                                      } else {
+                                          
+                                          NSDictionary *userInfo = @{
+                                                                     @"description" : [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]
+                                                                     };
+                                          
+                                          error = [NSError errorWithDomain:MTErrorHandlingErrorDomain
+                                                                      code:statusCode
+                                                                  userInfo:userInfo];
+                                          
+                                      }
+                                      
+                                      weakCompletionHandler(result, error);
+                                  }];
+    [task resume];
 }
-
 
 -(NSDictionary *)convertDataToJSON:(NSData *)data withError:(NSError **)error {
 
@@ -50,133 +61,6 @@ enum { kTimeout = 60 };
 //    DLog(@"response: %@", string);
     
     return [result objectFromJSONDataWithParseOptions:JKParseOptionNone error:error];
-    
-}
-
-#pragma mark - Connection
-
--(void) connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
-    
-    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
-        
-    error_ = NO;
-    [self stopTimer];
-    
-//    DLog(@"ETHttpRequest result: %@", result);
-//    DLog(@"ETHttpRequest status code: %ld", (long)statusCode_);
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-        if (self.requestCompletionBlock != nil) {
-            self.requestCompletionBlock(nil , error);
-            _requestCompletionBlock = nil;
-        }
-    });
-}
-
--(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
-
-    // reset data
-    [response_ setLength:0];
-    
-    // check status code and if 200, set data, else set error
-    statusCode_ = [((NSHTTPURLResponse*)response) statusCode];
-}
-
--(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-    
-    // add new data
-    [response_ appendData:data];
-}
-
--(void)connectionDidFinishLoading:(NSURLConnection *)connection
-{
-    
-    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
-
-    error_ = NO;
-    [self stopTimer];
-
-    if (self.requestCompletionBlock != nil) {
-        
-        NSError *error = nil;
-        NSDictionary *result;
-        
-        if (statusCode_ == 200) {
-            result = [self convertDataToJSON:response_ withError:&error];
-        } else {
-            
-            NSDictionary *userInfo = @{
-                                       @"description" : [[NSString alloc] initWithData:response_ encoding:NSUTF8StringEncoding]
-                                       };
-            
-            error = [NSError errorWithDomain:MTErrorHandlingErrorDomain
-                                        code:statusCode_
-                                    userInfo:userInfo];
-            
-        }
-        
-//        DLog(@"ETHttpRequest result: %@", result);
-//        DLog(@"ETHttpRequest status code: %ld", (long)statusCode_);
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if (self.requestCompletionBlock) {
-                self.requestCompletionBlock(result, error);
-                _requestCompletionBlock = nil;
-            }
-        });
-        
-    }
-}
-
-#pragma mark - Timer
-
-//
-// Start timer for catch timeout error
-//
--(void) startTimer {
-    
-//    DLog(@"startTimer");
-    
-    timer_ = [NSTimer 
-               scheduledTimerWithTimeInterval:kTimeout target:self 
-               selector:@selector(forceTimeout) userInfo:nil repeats:NO];
-    
-}
-
-//
-// Stop timer
-//
--(void) stopTimer {
-    
-//    DLog(@"stopTimer");
-    
-    if (timer_) {
-        [timer_ invalidate];
-        timer_ = nil;
-    }
-    
-}
-
-//
-//
-//
--(void) forceTimeout {
-    
-//    DLog(@"forceTimeout");
-    
-    if (connection_) {
-        
-        [connection_ cancel];
-        connection_ = nil;
-        
-        NSMutableDictionary *errorDetail = [NSMutableDictionary dictionary];
-        [errorDetail setValue:@"Failed to load data (timeout)" forKey:NSLocalizedDescriptionKey];
-        
-        // TODO: check domain
-        NSError *error = [NSError errorWithDomain:request_.URL.host code:1032 userInfo:errorDetail];
-        [self connection:connection_ didFailWithError:error];
-        
-    }
     
 }
 
